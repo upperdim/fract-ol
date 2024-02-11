d799b9902090ad183fac647f94b52822fb5d5f51
upperdim <45048352+upperdim@users.noreply.github.com>
2024-02-11 05:27:38 +0100

    exit closes window too

--- src/util/exit_error.c
+++ src/util/exit_error.c
@@ -6,18 +6,22 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 16:31:02 by tunsal            #+#    #+#             */
-/*   Updated: 2023/12/19 16:46:28 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/02/10 23:40:18 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/fractol.h"
 
-void	exit_error(const char *err_msg)
+void	exit_error(t_frac *frac, const char *err_msg)
 {
+	if (frac->window != NULL)
+	{
+		mlx_close_window(frac->window);
+	}
 	if (err_msg != NULL)
 	{
 		ft_putstr_fd((char *) err_msg, FD_STDERR);
 		ft_putchar_fd('\n', FD_STDERR);
 	}
-	exit (EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 }
