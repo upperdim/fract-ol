90c829348dbbf19a8c2353d0be637162bbe849f2
upperdim <45048352+upperdim@users.noreply.github.com>
2024-02-19 15:38:01 +0100

    fix format

--- src/fractals/move.c
+++ src/fractals/move.c
@@ -6,7 +6,7 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 17:54:35 by tunsal            #+#    #+#             */
-/*   Updated: 2024/02/19 14:02:36 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/02/19 14:07:22 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,18 @@ void	move(t_frac *frac, int direction)
 	{
 		frac->x_start += delta_x;
 		frac->x_end += delta_x;
-	} else if (direction == LEFT)
+	}
+	else if (direction == LEFT)
 	{
 		frac->x_start -= delta_x;
 		frac->x_end -= delta_x;
-	} else if (direction == UP)
+	}
+	else if (direction == UP)
 	{
 		frac->y_start += delta_x;
 		frac->y_end += delta_x;
-	} else if (direction == DOWN)
+	}
+	else if (direction == DOWN)
 	{
 		frac->y_start -= delta_x;
 		frac->y_end -= delta_x;
