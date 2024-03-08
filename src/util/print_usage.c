5fb7ebf9f3b2e0f4c1392e266d0698a152bcd752
upperdim <45048352+upperdim@users.noreply.github.com>
2024-03-08 11:11:46 +0100

    update usage message

--- src/util/print_usage.c
+++ src/util/print_usage.c
@@ -6,7 +6,7 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 23:36:15 by tunsal            #+#    #+#             */
-/*   Updated: 2024/03/08 08:14:59 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/03/08 11:08:09 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ void	print_usage(void)
 	ft_printf \
 	("usage:\t./fract-ol <fractal type> [julia value 1] [julia value 2]\n");
 	ft_printf("\t\t(julia values must be between -2 and 2)\n");
-	ft_printf("fractal types: m = mandelbrot, j = julia, b = burning ship, \
-i = interactive julia\n");
+	ft_printf("fractal types: m = mandelbrot, j = julia, c = collatz \
+conjecture, i = interactive julia\n");
 }
